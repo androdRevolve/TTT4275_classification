6e3abe6b67373327ddf709ff6ca069b3aa23221a
kvello <markus.kv1@gmail.com>
2023-04-16 15:34:59 +0200

    Adds iris calssification code

---
+++ softmaxGradient.m
@@ -0,0 +1,13 @@
+function gradSoft = softmaxGradient(x)
+    sigma = softmax(x);
+    gradSoft = zeros(size(x));
+    for i = 1:length(x)
+        for j = 1:length(x)
+            if i == j
+                gradSoft(i,j) = sigma(i)*(1-sigma(i));
+            else
+                gradSoft(i,j) = sigma(i)*sigma(j);
+            end
+        end
+    end
+end
