7dd708c8d8b037fc00111d46f6026e66b6c393e1
Andreas Rodahl <andreas.rodahl@revolve.no>
2023-04-19 21:09:08 +0200

    Complete task 1 and 2

--- src/mnist/Task_1.m
+++ src/mnist/Task_1.m
@@ -4,9 +4,11 @@
 % find the training reference with the smallest distance and classify as
 % that target
 %% Init
-num_references = 2000;
-num_samples = 1000;
+num_references = 1000;
+num_samples = 10000;
 %% Run classification using the first num_references references and the first num_samples test features
+tic;
+
 mtrx_references = trainv(1:num_references,:)';
 vec_targets = trainlab(1:num_references);
 
@@ -18,7 +20,7 @@ for test_samp = 1:num_samples
     [dist_min,ind_min] = min(distances);
     classes(test_samp) = vec_targets(ind_min);
 end
-
+toc;
 %% Evaluate performance
 
 % Find confusion matrix and error rate for the test set.
@@ -31,22 +33,25 @@ num_correct = sum(is_equal);
 error_rate = (num_samples-num_correct)/num_samples * 100;
 
 % Display
-disp(strcat("The error rate is ", num2str(error_rate), "%."));
+disp(strcat("The error rate for the unclustered NN is ", num2str(error_rate), "%."));
+
+figure;
 hmo = heatmap({'0', '1', '2,' '3', '4', '5', '6', '7', '8', '9'}, {'0', '1', '2,' '3', '4', '5', '6', '7', '8', '9'}, mtrx_confusion);
-hmo.Title = "NN confusion matrix";
+hmo.Title = "Unclustered NN confusion matrix";
 hmo.XLabel = "True value";
 hmo.YLabel = "Classification";
+hmo.ColorScaling = 'log';
 
 %% Plot some misclassified pictures
 % Plot the first 5 misclassified pictures
 num_plotted = 0;
-i = 100;
+i = 5000;
 while num_plotted < 5
     if(~is_equal(i))
         % Convert picture from vector to matrix
         mtrx_pic = zeros(col_size,row_size); mtrx_pic(:) = testv(i,:);
         image(mtrx_pic');
-        num_plotted = num_plotted+1;
+         num_plotted = num_plotted+1;
     end
     i = i+1;
 end
@@ -54,7 +59,7 @@ end
 %% Plot some correctly classified pixtures
 % Plot the first 5 correctly classified pixture
 num_plotted = 0;
-i = 100;
+i = 5000;
 while num_plotted < 5
     if(is_equal(i))
         % Convert picture from vector to matrix
@@ -64,6 +69,20 @@ while num_plotted < 5
     end
     i = i+1;
 end
+
+%% Conclusion
+% The unclustered NN classifier has an error rate of 13.1% on the whole
+% dataset (10k samples) using 1k references. The error rate is quite
+% sensitive to the number of references, exemplified by an error rate of
+% 9.72% When using 2k references.
+% The runtime of the classifier is very long (135.45 s) when
+% using 1000 references. When increasing the number of references to 2k,
+% the runtime is 474.96 s.
+
+% In particular, the classifier struggles with the
+% distinction between the class pairs (4,9), (3,5), (8,5). Which makes sense as there is
+% significant overlap in the shapes of these digits, and variation in
+% handwriting can decide the margins.
 
 disp('..done');
 
--- src/mnist/Task_2.m
+++ src/mnist/Task_2.m
@@ -1,16 +1,16 @@
 %% Task 2
 % Use clustering to produce a small(er) set of templates for each class.
 N_clusters = 64;
-N_vectors_per_class = 6000;
 N_classes = 10;
 
 %% a)  Perform clustering of the 6000 training vectors for each class into M = 64 clusters.
 % Separate training data into matrix based on class
+tic;
 
 load('class_clusters.mat');
 % arr_class_clusters = cell(1,10);
 % for i = 1:length(trainlab)
-%     arr_class_clusters{trainlab(i)+1} = [arr_class_clusters{trainlab(i)+1}; trainv(i,:)];
+%     arr_class_clusters{trainlab(i)+1}(end+1,:) = trainv(i,:);
 % end
 
 load('arr_clusters.mat');
@@ -19,11 +19,13 @@ load('arr_clusters.mat');
 %     [idx,C] = kmeans(arr_class_clusters{i}, N_clusters);
 %     arr_clusters{i} = C;
 % end
+toc;
+
 
 %% Perform classification
 % Run classification on one class, find the
 % minimum distances for all feature vectors
-
+tic;
 for i = 1:N_classes
     distances = dist(arr_clusters{i}, testv');
     min_distances(i,:) = min(distances,[],1);
@@ -32,6 +34,7 @@ end
 % Classify by choosing the class with the minimum distance
 [~,class] = min(min_distances,[],1);
 class = class'-1;
+toc;
 
 %% b) Find the confusion matrix and the error rate for the NN classifier using these M = 64
 % templates pr class. Comment on the processing time and the performance relatively to
@@ -47,14 +50,108 @@ error_rate = num_errors / length(testlab) * 100;
 mtrx_confusion = calc_confusion_matrix(testlab, class);
 
 % Display
-disp(strcat("The error rate is ", num2str(error_rate), "%."));
-hmo = heatmap({'0', '1', '2,' '3', '4', '5', '6', '7', '8', '9'}, {'0', '1', '2,' '3', '4', '5', '6', '7', '8', '9'}, mtrx_confusion);
-hmo.Title = "NN confusion matrix";
-hmo.XLabel = "True value";
-hmo.YLabel = "Classification";
+disp(strcat("The error rate for the clustered NN is ", num2str(error_rate), "%."));
+
+figure;
+heatmap_NN = heatmap({'0', '1', '2,' '3', '4', '5', '6', '7', '8', '9'}, {'0', '1', '2,' '3', '4', '5', '6', '7', '8', '9'}, mtrx_confusion);
+heatmap_NN.Title = "NN confusion matrix";
+heatmap_NN.XLabel = "True value";
+heatmap_NN.YLabel = "Classification";
+heatmap_NN.ColorScaling = 'log';
 
 %% c) Now design a KNN classifier with K=7. Find the confusion matrix and the error rate and
 % compare to the two other systems.
+tic;
+K_knn = 7;
+
+% Collect all distances in a single matrix
+distances = zeros(N_classes*N_clusters, length(testlab));
+for i = 1:N_classes
+    distances(N_clusters*(i-1)+1:N_clusters*i,:) = dist(arr_clusters{i}, testv');
+end
+
+%% Classify these neighbors from their indices
+% Identify the K nearest neighbours of each feature vector
+[~,I] = mink(distances, K_knn, 1);
+classes_neighbors = floorDiv(I,N_clusters+1);
+
+classes = zeros(length(testlab), 1);
+% Majority vote; identify the class with the highest number of neighbors,
+% if two classes get equal amount of votes, choose the closest neighbor
+for i=1:length(testlab)
+    % Determine the count of each class
+    [votes, class_val] = groupcounts(classes_neighbors(:,i));
+    % Find the maximum votes
+    [max_votes, ind] = max(votes);
+    % Find number of contestants
+    is_contestant = votes == max_votes;
+    N_contestants = sum(is_contestant);
+    if N_contestants > 1
+        % Determine what classes are contestants
+        contestants = class_val(is_contestant);
+        % Find the shortest distance to each of the classes
+        contestant_distance = zeros(N_contestants, 1);
+        for j=1:N_contestants
+            contestant_distance(j) = min(dist(arr_clusters{contestants(j)+1}, testv(i,:)'));
+        end
+        % Classify as the class with the lowest distance
+        [~, ind] = min(contestant_distance);
+        classes(i) = contestants(ind);
+    else
+        classes(i) = class_val(ind);
+    end
+end
+toc;
+%% Evaluate performance
+
+% Error rate
+is_equal = classes == testlab;
+num_correct = sum(is_equal);
+num_errors = length(testlab)-num_correct;
+error_rate = num_errors / length(testlab) * 100;
+
+% Confusion matrix
+mtrx_confusion = calc_confusion_matrix(testlab, classes);
+
+% Display
+disp(strcat("The error rate for the clustered KNN classifier is ", num2str(error_rate), "%."));
+figure;
+heatmap_KNN = heatmap({'0', '1', '2,' '3', '4', '5', '6', '7', '8', '9'}, {'0', '1', '2,' '3', '4', '5', '6', '7', '8', '9'}, mtrx_confusion);
+heatmap_KNN.Title = "KNN confusion matrix";
+heatmap_KNN.XLabel = "True value";
+heatmap_KNN.YLabel = "Classification";
+heatmap_KNN.ColorScaling = 'log';
+
+%% Conclusion
+% Interestingly, the NN classifier outperforms the KNN classifier. Looking
+% at the results (define test = (class == testlab & classes ~= testlab)) it
+% appears that the reason is that the KNN classifier sometimes gets
+% "infiltrated" by wrong neighbors, e.g. "9" is correct and closest, but
+% "9" gets outvoted by "7". The conclusion: for this dataset, the
+% "MAP"-classifier outperforms the KNN; we should rather trust the single
+% closest neighbor than the K closest.
+
+% The KNN classifier outperforms the NN classifier without clustering
+% implemented in task 1.
+
+% Clustering has a big impact on the runtime of the classification. With
+% clustering, some time is spent on the clustering algorithm that
+% initializes the references. When the references are done, the computation
+% is much faster for each new feature x, as the total number of references
+% is much lower (640 vs 1k in the default case).
+
+% Timing:
+% 64 clusters
+% Reference sorting and clustering: 98.30 seconds 
+% Classification: 11.42 seconds (NN), 12.1 seconds (KNN)
+
+% 32 clusters
+% Reference sorting and clustering: 99.36 seconds
+% Classification: 5.85 seconds (NN), 6.77 seconds (KNN)
+
+% Error rates:
+% 64 clusters: 4.6% (NN), 6.02% (KNN)
+% 32 clusters: 5.26% (NN), 8.95% (KNN)
 
 
 disp('..done');
